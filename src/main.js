09addf5336a96bfd9b185474e499fe1229097283
miiito301 <ikaruga0131@gmail.com>
2025-07-18 01:28:36 +0200

    Delete src directory

--- src/main.js
+++
@@ -1,9 +0,0 @@
-
-
-/*main.js */
-import { createApp } from 'vue'
-import App from './App.vue'
-import router from './router'  // ここでルーターを読み込む
-import "./assets/output.css"
-
-createApp(App).use(router).mount('#app')
